570ab7579e5c99e720e84ee804a262b66f5368c6
Odilon <woreoaf@gmail.com>
2020-01-17 18:29:12 -0300

    start frontend web

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./global.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="app">
+      <aside></aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
